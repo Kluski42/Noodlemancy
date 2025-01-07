1b05084f86e6ec9ec1fd76e1a5892ffa1a1bb872
Wet Noodle <kluski42@gmail.com>
2025-01-07 18:18:08 -0500

    Made creaking eye orientable (missing correct texture rotation)

--- src/main/java/net/wetnoodle/noodlemancy/block/CreakingEyeBlock.java
+++ src/main/java/net/wetnoodle/noodlemancy/block/CreakingEyeBlock.java
@@ -7,9 +7,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -44,6 +42,16 @@ public class CreakingEyeBlock extends BaseEntityBlock {
     }
 
     @Override
+    protected @NotNull BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    @Override
+    protected @NotNull BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+    }
+
+    @Override
     protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
         return CODEC;
     }
@@ -67,18 +75,47 @@ public class CreakingEyeBlock extends BaseEntityBlock {
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
-        return createTickerHelper(blockEntityType, NMBlockEntityTypes.CREAKING_EYE, CreakingEyeBlockEntity::tick);
+        return level.isClientSide() ? null : createTickerHelper(blockEntityType, NMBlockEntityTypes.CREAKING_EYE, CreakingEyeBlockEntity::tick);
     }
 
     // Fun stuff
 
+    public static void updatePower(Level level, BlockPos blockPos, BlockState blockState, int newPower) {
+        if (blockState.getValue(POWER) != newPower) {
+            level.setBlock(blockPos, blockState.setValue(POWER, newPower).setValue(EYES, (int) Math.ceil(newPower / 5.0)), Block.UPDATE_ALL);
+            CreakingEyeBlock.updateNeighbours(level, blockPos, blockState);
+        }
+    }
+
+    private static void updateNeighbours(Level level, BlockPos blockPos, BlockState blockState) {
+        Block block = blockState.getBlock();
+        Direction direction = blockState.getValue(FACING).getOpposite();
+        level.updateNeighborsAt(blockPos, block);
+        level.updateNeighborsAt(blockPos.relative(direction), block);
+    }
+
     @Override
-    protected int getSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
-        return blockState.getValue(POWER);
+    protected void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean bl) {
+        if (!blockState.is(blockState2.getBlock())) {
+            if (!level.isClientSide && (blockState.getValue(POWER) > 0)) {
+                updateNeighbours(level, blockPos, blockState.setValue(POWER, 0));
+            }
+        }
+        super.onRemove(blockState, level, blockPos, blockState2, bl);
     }
 
     @Override
     protected boolean isSignalSource(BlockState blockState) {
         return true;
+    }
+
+    @Override
+    protected int getDirectSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
+        return blockState.getSignal(blockGetter, blockPos, direction);
+    }
+
+    @Override
+    protected int getSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
+        return blockState.getValue(FACING) == direction ? blockState.getValue(POWER) : 0;
     }
 }
